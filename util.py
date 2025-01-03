eccec61089830ed5c866042334cb189b20dbecb4
mark zhong <pzsx005@gmail.com>
2025-01-03 17:29:10 +1300

    Update util.py

--- util.py
+++ util.py
@@ -27,15 +27,30 @@ def load_data(data_folder, data_name, label_name):
             imgpath.read(), np.uint8, offset=16).reshape(len(y_data), 28, 28)
     return (x_data, y_data)
 
-def get_data():
-    # load local mnist
-    folder = '/data1/ywh/MNIST_data'
-    train_data = 'train-images-idx3-ubyte.gz' 
-    train_label = 'train-labels-idx1-ubyte.gz'
-    test_data = 't10k-images-idx3-ubyte.gz'
-    test_label = 't10k-labels-idx1-ubyte.gz'
-    # dealdataset
-    trainDataset = DealDataset(folder, train_data, train_label,transform=transforms.ToTensor())
-    testDataset = DealDataset(folder, test_data, test_label,transform=transforms.ToTensor())
+def get_data(data_dir='./data', batch_size=64):
+    """
+    Downloads and loads the MNIST dataset using PyTorch's torchvision library.
 
-    return trainDataset, testDataset
+    Args:
+        data_dir (str): Directory where the MNIST data will be stored.
+        batch_size (int): Batch size for the DataLoader.
+
+    Returns:
+        train_loader (torch.utils.data.DataLoader): DataLoader for training dataset.
+        test_loader (torch.utils.data.DataLoader): DataLoader for testing dataset.
+    """
+    # Define transformations
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # Convert PIL image to PyTorch tensor
+        transforms.Normalize((0.5,), (0.5,))  # Normalize to mean 0, variance 1
+    ])
+    
+    # Download and load MNIST datasets
+    train_dataset = datasets.MNIST(root=data_dir, train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(root=data_dir, train=False, download=True, transform=transform)
+
+    # Create DataLoaders
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
